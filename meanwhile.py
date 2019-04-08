5c12b5271ac0ee3710ea8545d28900ad7a300adc
Radomir Dopieralski <openstack@sheep.art.pl>
2019-04-08 21:36:51 +0200

    Add some comments

--- meanwhile.py
+++ meanwhile.py
@@ -5,11 +5,13 @@ futures = {}
 
 
 class Future:
+    """A trivial future, that is immediately finished."""
     def done(self):
         return True
 
 
 class SleepFuture(Future):
+    """A future that is finished after set time."""
     def __init__(self, when):
         self.when = when
 
@@ -18,6 +20,7 @@ class SleepFuture(Future):
 
 
 class PinChangeFuture(Future):
+    """A future that is finished when a pin value changes."""
     def __init__(self, pin):
         self.pin = pin
         self.last_value = pin.value
@@ -27,30 +30,38 @@ class PinChangeFuture(Future):
 
 
 def watch_pin(pin):
+    """Asynchronously wait until a pin changes value."""
     yield PinChangeFuture(pin)
 
 
 def sleep(seconds):
+    """Asynchronously wait until given time passes."""
     yield SleepFuture(time.monotonic() + seconds)
 
 
 def start_soon(*awaitables):
+    """Schedule awaitables to be executed."""
     for awaitable in awaitables:
         futures[awaitable] = Future()
 
 
 def run(*awaitables):
+    """Run all scheduled and specified awaitables until they all finish."""
     start_soon(*awaitables)
     while futures:
+        # Find an awaitable that is not blocked on a future.
         for awaitable, future in futures.items():
             if future.done():
                 break
         else:
             continue
+        # Remove that future from the list.
         del futures[awaitable]
+        # Run the awaitable until it hits another future or finishes.
         try:
             future = next(awaitable)
         except StopIteration:
             pass
         else:
+            # Add the new future to the list.
             futures[awaitable] = future
